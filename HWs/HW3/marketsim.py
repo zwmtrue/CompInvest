09972a209c8a049f5a957ee2c0f579dae7c21a37
zhuwn <zhuwn@mail.uc.edu>
2016-04-28 14:54:51 -0400

    marketsim.py working properly
    In last version, I had some confusion on how ts_cash works, now I see it
    just stores daily flows of cash.

--- HWs/HW3/marketsim.py
+++ HWs/HW3/marketsim.py
@@ -10,85 +10,104 @@ import pandas as pd
 import numpy as np
 import math
 
+class order:
+    """Class obj for order"""
+    def  __init__(self, orderinfo ):
+        info = orderinfo#.split()
+        self.date =  dt.datetime(int(info[0]),int(info[1]),\
+        int(info[2]),16,00)
+        self.symbol = info[3]
+        
+        
+        info[4] = info[4].strip()
+        if info[4] == 'Buy':
+            self.exchange = 'buy'
+            
+            self.volume = int(info[5])
+        elif info[4] == 'Sell':
+            self.exchange = 'sell'
+            self.volume = - int(info[5])
+        else:
+            self.volume = info[4]#float('nan')
+    
+    def __str__(self):
+        s = str(self.date)+'   '+ self.exchange +\
+        '  '+self.symbol +'  in quantity of  '+ str(abs(self.volume))
+        return s
+    
 if __name__ == '__main__':
     dataobj = da.DataAccess('Yahoo', cachestalltime=0)
     dt_timeofday = dt.timedelta(hours=16)
     starting_cash = 1000000
 #step 1
-    ordercsv = 'C:\Users\William\PycharmProjects\CompInvest\HWs\HW3\orders.csv'
-    orderbook = csv.reader(open(ordercsv,'rU'),delimiter = ',')
+    ordercsv = open('.\orders.csv','rU')
+    orderbook = csv.reader(ordercsv,delimiter = ',')
     ls_alldates = list()
     ls_symbols = list()
+    ls_orders = list()
 
-    for order in orderbook:
-        ls_alldates.append(dt.datetime(int(order[0]),int(order[1]),int(order[2]),16,00))
-        ls_symbols .append(order[3])
-
-
+    for orderinfo in orderbook:
+        thisorder = order(orderinfo)
+        ls_orders.append(thisorder)
+        ls_alldates.append(thisorder.date)
+        ls_symbols.append(thisorder.symbol)
+    #duplicates removal
+    ls_symbols =   list(set(ls_symbols))
     ls_alldates =  sorted(list(set(ls_alldates)))
     dt_first = min(ls_alldates)
     dt_last = max(ls_alldates)
-    ls_symbols =   list(set(ls_symbols))
     dt_start_read = dt_first
     dt_end_read = dt_last + dt.timedelta(days=1)
 #step2
     dt_timeofday = dt.timedelta(hours=16)
-    ldt_timestamps = du.getNYSEdays(dt_start_read, dt_end_read, dt_timeofday)
+    ldt_timestamps = du.getNYSEdays(dt_start_read,\
+    dt_end_read, dt_timeofday)
     ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
     ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
     d_data = dict(zip(ls_keys, ldf_data))
     df_close = d_data['close']
+    
     init_val = np.zeros((len(ls_alldates),len(ls_symbols)))
 #step 3
-    df_trade = pd.DataFrame(init_val,index = ls_alldates,columns = ls_symbols)
-    orderbook = csv.reader(open(ordercsv,'rU'),delimiter = ',')
-    for order in orderbook:
-        date = dt.datetime(int(order[0]),int(order[1]),int(order[2]),16,00)
-        symbol = order[3]
-        vol = int(order[5])
-        if order[4] is 'Sell':
-            vol = -vol
-        id = ls_alldates.index(date)
-        isy = ls_symbols.index(symbol)
-        fin_val = df_trade.set_value(date, symbol, vol )
-    #step 4
+    df_trade = pd.DataFrame(init_val,index = ls_alldates,\
+    columns = ls_symbols)
+    ordercount = 0
+    for one_order in ls_orders:
+        try:
+            alreadytrade = df_trade[ one_order.symbol][one_order.date]
+        except:
+            alreadytrade = 0
+        df_trade.set_value(one_order.date, one_order.symbol, \
+        one_order.volume + alreadytrade )
+#step 4
     ls_symbols.append("_CASH")
-    ts_cash = pd.TimeSeries(np.zeros(len(ls_alldates)),index = ls_alldates)
-    orderbook = csv.reader(open(ordercsv, 'rU'), delimiter=',')
-    ts_cash[dt_first]=starting_cash
-    for order in orderbook:
-        date= dt.datetime(int(order[0]),int(order[1]),int(order[2]),16,00)
-        if date == dt_first:
-            lastdate=date
+    ts_cash = pd.Series(np.zeros(len(ls_alldates)),index = ls_alldates)
+    for today in ls_alldates:
+        orders_in_today = [o for o in ls_orders if o.date == today]
+        expense = 0
+        for o in orders_in_today:
+            expense += df_close[o.symbol][today]*o.volume
+        if today == dt_first:
+            ts_cash[today]= starting_cash - expense
         else:
-            id = ls_alldates.index(date)
-            lastdate = ls_alldates[id-1]
-
-        vol = int(order[5])
-        symbol = order[3]
-        if order[4] is 'Sell':#Sell add cash
-            vol = -vol
-       # price = d_data['close'][symbol][date]
-        price = df_close[symbol][date]
-        traded = ts_cash[lastdate]
-       # print 'traded, price of stock, volume, date'
-        #print traded,'\t',price,symbol,'\t',vol,date,traded - price*vol
-        ts_cash[date] = traded - price*vol
-   # print ts_cash
+            ts_cash[today]=   -expense 
+            
+        
     df_close['_CASH']=1.0
     df_trade['_CASH'] = ts_cash
-
-    #step 5
+#step 5
     df_holding = df_trade.cumsum()
 
     print 'holding\n',df_holding
-    val = list()
+    val_output = open('values.csv','wb')
+    ts_fund = pd.Series()
+    writetocsv = csv.writer(val_output,delimiter = ',')
     for date in ls_alldates:
         curval = 0
         for symbol in ls_symbols:
             curval += df_holding[symbol][date]*df_close[symbol][date]
-        val.append(curval)
-        print date,curval
-
-    #print val
-
+        ts_fund[date] = curval
+        rows_to_enter = [date.year,date.month,date.day,curval]
+        writetocsv.writerow(rows_to_enter)
+    val_output.close()
+    ordercsv.close()
